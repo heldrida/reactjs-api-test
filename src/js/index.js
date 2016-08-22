3d1c8961cc2f3e4e9997f37c1df2a42adec25a67
Helder <heldrida@gmail.com>
2016-08-22 23:05:28 +0100

    created router reducer

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { syncHistoryWithStore } from 'react-router-redux'
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import promise from 'redux-promise';
@@ -9,10 +10,11 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 const store = createStoreWithMiddleware(reducers);
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
 	<Provider store={ store }>
-		<Router routes={ routes } />
+		<Router history={ history } routes={ routes } />
 	</Provider>,
     document.getElementById('app')
 );
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux'
 import youtubeReducer from './youtube_reducer';
 
-
 const rootReducer = combineReducers({
-	videos: youtubeReducer
+	videos: youtubeReducer,
+	routing: routerReducer
 });
 
 export default rootReducer;
