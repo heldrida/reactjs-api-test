f66e052a54b25d3048c6249785bbf27d5b0b2dac
Helder <heldrida@gmail.com>
2016-08-22 22:56:24 +0100

    setting up redux, wrapped the router into the provider

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import App from './containers/app';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 
 ReactDOM.render(
-	<App />,
+	<Provider>
+		<Router routes={ routes } />
+	</Provider>,
     document.getElementById('app')
 );
