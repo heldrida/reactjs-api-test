5968a633cfe09957a719ca1d2792e80e22eb7c41
Helder <heldrida@gmail.com>
2016-08-22 21:41:02 +0100

    created the webpack dev configuration; kept simple

---
+++ webpack.dev.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/dev-server',
+		'./app/index.js'
+	],
+	output: {
+		path: __dirname + '/build',
+		filename: "index_bundle.js"
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
+            { test: /\.scss$/, loader: 'style!css!sass' }
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
+};
