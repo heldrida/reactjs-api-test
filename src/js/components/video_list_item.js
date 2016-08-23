df8e76b96cd379f339aaccc6988e215ad257105a
Helder <heldrida@gmail.com>
2016-08-23 02:48:39 +0100

    quick styles for the video list

--- src/js/components/video_list_item.js
+++ src/js/components/video_list_item.js
@@ -11,7 +11,8 @@ const VideoListItem = ({ video, onVideoSelect }) => {
 			</div>
 			<div className="media-body">
 				<div className="media-heading">
-				{ video.snippet.title }
+					<h4>{ video.snippet.title }</h4>
+					<p>{ video.snippet.description }</p>
 				</div>
 			</div>
 		</li>
